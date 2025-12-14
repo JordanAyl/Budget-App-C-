c9fb317d09b28c18d1499ef43caa25cca34398a6
JordanAyl <jordandayling@gmail.com>
2025-12-13 20:54:51 -0800

    Qstring conversion to std String. Qvector conversion to std vector.

--- src/file.cpp
+++ src/file.cpp
@@ -7,6 +7,13 @@
 int File::printToFile(const QVector<Expense> &vec1,const QVector<Income> &vec2,const QString &leftOverAmount)
 {
 
+    //Qvector is change to a normal vector.
+    vector<Expense> expenses(vec1.begin(), vec1.end());
+    //Qvector is change to a normal vector.
+    vector<Income> income(vec2.begin(), vec2.end());
+    //QString is change to a normal string.
+    string leftover = leftOverAmount.toUtf8().toStdString();
+
     cout << "Vector<Expense> 1 elements:\n";
         for (auto x : vec1) {
             cout << x << " ";
